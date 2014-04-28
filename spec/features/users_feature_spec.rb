8a018b566834ef0f130b42cea331bfb932e305b2
Rick Brunstedt <rbrunstedt@gmail.com>
2014-04-28 11:02:02 +0100

    Devise

---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper.rb'
+
+describe "sign up" do
+  it "redirect to homepage" do
+    visit '/users/sign_up'
+    fill_in 'Email', with: 'a@a.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Welcome user!'
+  end
+  
+end
