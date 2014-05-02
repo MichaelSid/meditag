24a656522d6252aba27275507de5182cf14f111a
Hannah <hannah_wight@hotmail.com>
2014-05-02 13:25:50 +0100

    added faq and contact pages, formatted buttons to min-width, logo now leads home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/contact'
+
+  get 'static_pages/about'
+
+  get 'static_pages/faq'
+
   devise_for :users, controllers: { :registrations => "registration" }
   
   root 'home#index'
