66366b4c2a671e38ce218f166d1c0a8ed6ad8eb2
RichardC <richardcurteis@hotmail.co.uk>
2014-05-03 17:00:50 +0100

    working on authorisation process

--- app/controllers/idverifies_controller.rb
+++ app/controllers/idverifies_controller.rb
@@ -1,7 +1,7 @@
 class IdverifiesController < ApplicationController
 
   def new
-    # @uuid = params['tag-uuid'].to_s
+    # @uuid = params['tag-uuid'].to_s\
     # verify_uuid?
     # uuid_assigned_to_user?
   end
@@ -12,23 +12,16 @@ class IdverifiesController < ApplicationController
   end
 
   def verification_functions
-    verify_uuid_length?
-    uuid_assigned_to_user?
+    if verify_uuid_length? && uuid_assigned_to_user?
+
   end
 
   def verify_uuid_length?
-    @uuid.length == 36 ? flash[:notice] = 'Correct length' : flash[:error] = 'Incorrect length'
+    @uuid.length == 36 #? flash[:notice] = 'Correct length' : flash[:error] = 'Incorrect length'
   end
 
   def uuid_assigned_to_user?
-    User.exists?(uuid: @uuid) ? flash[:error] = 'ID already assigned to user' : flash[:notice] = 'ID not assigned to user'
-  end
-
-  def uuid_is_nil?
-    if @uuid == nil
-      redirect_to '/idverify/new'
-      flash[:error] = 'Value cannot be nil'
-    end
+    User.exists?(uuid: @uuid) #? flash[:error] = 'ID already assigned to user' : flash[:notice] = 'ID not assigned to user'
   end
 
 end
