b13b7cfa950da3a3deb9c39676b2e513874ab560
Rick Brunstedt <rbrunstedt@gmail.com>
2014-05-08 12:38:20 +0100

    twilio fixed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,14 @@
 class MessagesController < ApplicationController
 
 	def create
+    @user = User.find(params[:user_id])
+
 		@twilio_client = Twilio::REST::Client.new(Rails.application.secrets.twilio_sid, Rails.application.secrets.twilio_tkn)
 
-		@twilio_client.account.messages.create(to: "#{current_user.emergencycontact.contactnr}", from: Rails.application.secrets.twilio_num, body: "#{current_user.profile.firstname} #{current_user.profile.lastname}'s medicode was accessed")
+		@twilio_client.account.messages.create(to: "#{@user.emergencycontact.contactnr}", from: Rails.application.secrets.twilio_num, body: "#{@user.profile.firstname} #{@user.profile.lastname}'s medicode was accessed")
 		
 		flash[:notice] = 'Text sent!'
-		redirect_to "/emergency/#{current_user.id}"
+		redirect_to "/emergency/#{@user.id}"
 	end
 
 
